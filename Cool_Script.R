36b31bbed8be4e24d730be3eed9a92496e01c44c
Kelly <kelly@hellomister.com.au>
2018-08-29 13:11:35 +1000

    WeatherMerge
    Merge example from class 29 August 2018

--- Cool_Script.R
+++ Cool_Script.R
@@ -3,15 +3,19 @@
 
 # install.packages("tidyverse")
 
+
+
 library(tidyverse)
 
+# manipulate data set
 
 diamonds2 <- diamonds %>% 
-        filter(color == G & depth > 60) %>% 
+        filter(color == "G" & depth > 60) %>% 
         group_by(clarity) %>% 
         arrange(clarity) %>% 
-        mutate(av_carat = mean(carot))
+        mutate(av_carat = mean(carat)) %>% 
+        mutate(med_carat = median(carat)) 
 
-ggplot(diamonds2, aes(car0t, price)) %>% 
-        geom_point(aes(colour=cut size=table, alpha=1/10)) +
+ggplot(diamonds2, aes(carat, price)) +
+        geom_point(aes(colour=cut, size=table, alpha=1/10)) 
 
